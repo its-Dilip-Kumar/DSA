68e93ecc0fb01f2592a062c2e81687727ba76b31
Dilip Kumar <157205347+its-Dilip-Kumar@users.noreply.github.com>
2026-01-31 12:04:16 +0530

    Time: 18 ms (68.00%) | Memory: 64.1 MB (6.05%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static boolean solve(int i,int j,String s,String p,Boolean[][] dp){
+        if(i<0 && j<0) return true;
+        if(i>=0 && j<0) return false;
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+                if(p.charAt(k)!='*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j]!=null) return dp[i][j];
+        
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            return dp[i][j]=solve(i-1,j-1,s,p,dp);
+        }else if(p.charAt(j)=='*'){
+            return dp[i][j]=solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
+        }
+        return false;
+    }
+    public boolean isMatch(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+        Boolean[][] dp=new Boolean[n][m];
+        return solve(n-1,m-1,s,p,dp);
+    }
+}
